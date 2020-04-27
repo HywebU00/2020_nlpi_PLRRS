b31b031f14925aa3a0ff48a72981c24f3288e64b
mingshuya <mingshuya@gmail.com>
2020-04-27 19:08:50 +0800

    暫存清單套版

--- js/customize.js
+++ js/customize.js
@@ -201,6 +201,204 @@ $(function() {
 
 });
 
+$(function() {
+    $('#modal4').hide();//先隱藏視窗
+    $('.modal4').after('<div class="modal_overlay"></div>'); //新增透明底
+    $('.modal4').prepend('<button type="button" class="close">關閉</button>');  //新增關閉按鈕
+    $('.modal_overlay').hide();  //隱藏透明底
+    //按鈕動作
+    $('#openModal4').click(function(e) {
+        $('.modal_overlay').fadeIn(100);
+        $('.modal4').fadeIn(100);
+        $('body').addClass('noscroll');
+        e.preventDefault();
+    });
+    //關閉function
+    function closeModal(){
+        $('#modal4').hide();
+        $('.modal_overlay').hide();
+        $('body').removeClass('noscroll');
+    }
+    //點選關閉按鈕及透明底都可關閉
+    $('.modal_overlay').click(closeModal);
+    $('.modal4 .close').click(closeModal);
+
+});
+
+$(function() {
+    $('#modal5').hide();//先隱藏視窗
+    $('.modal5').after('<div class="modal_overlay"></div>'); //新增透明底
+    $('.modal5').prepend('<button type="button" class="close">關閉</button>');  //新增關閉按鈕
+    $('.modal_overlay').hide();  //隱藏透明底
+    //按鈕動作
+    $('#openModal5').click(function(e) {
+        $('.modal_overlay').fadeIn(100);
+        $('.modal5').fadeIn(100);
+        $('body').addClass('noscroll');
+        e.preventDefault();
+    });
+    //關閉function
+    function closeModal(){
+        $('#modal5').hide();
+        $('.modal_overlay').hide();
+        $('body').removeClass('noscroll');
+    }
+    //點選關閉按鈕及透明底都可關閉
+    $('.modal_overlay').click(closeModal);
+    $('.modal5 .close').click(closeModal);
+
+});
+
+$(function() {
+    $('#modal6').hide();//先隱藏視窗
+    $('.modal6').after('<div class="modal_overlay"></div>'); //新增透明底
+    $('.modal6').prepend('<button type="button" class="close">關閉</button>');  //新增關閉按鈕
+    $('.modal_overlay').hide();  //隱藏透明底
+    //按鈕動作
+    $('#openModal6').click(function(e) {
+        $('.modal_overlay').fadeIn(100);
+        $('.modal6').fadeIn(100);
+        $('body').addClass('noscroll');
+        e.preventDefault();
+    });
+    //關閉function
+    function closeModal(){
+        $('#modal6').hide();
+        $('.modal_overlay').hide();
+        $('body').removeClass('noscroll');
+    }
+    //點選關閉按鈕及透明底都可關閉
+    $('.modal_overlay').click(closeModal);
+    $('.modal6 .close').click(closeModal);
+
+});
+
+$(function() {
+    $('#modal7').hide();//先隱藏視窗
+    $('.modal7').after('<div class="modal_overlay"></div>'); //新增透明底
+    $('.modal7').prepend('<button type="button" class="close">關閉</button>');  //新增關閉按鈕
+    $('.modal_overlay').hide();  //隱藏透明底
+    //按鈕動作
+    $('#openModal7').click(function(e) {
+        $('.modal_overlay').fadeIn(100);
+        $('.modal7').fadeIn(100);
+        $('body').addClass('noscroll');
+        e.preventDefault();
+    });
+    //關閉function
+    function closeModal(){
+        $('#modal7').hide();
+        $('.modal_overlay').hide();
+        $('body').removeClass('noscroll');
+    }
+    //點選關閉按鈕及透明底都可關閉
+    $('.modal_overlay').click(closeModal);
+    $('.modal7 .close').click(closeModal);
+
+});
+
+$(function() {
+    $('#modal8').hide();//先隱藏視窗
+    $('.modal8').after('<div class="modal_overlay"></div>'); //新增透明底
+    $('.modal8').prepend('<button type="button" class="close">關閉</button>');  //新增關閉按鈕
+    $('.modal_overlay').hide();  //隱藏透明底
+    //按鈕動作
+    $('#openModal8').click(function(e) {
+        $('.modal_overlay').fadeIn(100);
+        $('.modal8').fadeIn(100);
+        $('body').addClass('noscroll');
+        e.preventDefault();
+    });
+    //關閉function
+    function closeModal(){
+        $('#modal8').hide();
+        $('.modal_overlay').hide();
+        $('body').removeClass('noscroll');
+    }
+    //點選關閉按鈕及透明底都可關閉
+    $('.modal_overlay').click(closeModal);
+    $('.modal8 .close').click(closeModal);
+
+});
+
+$(function() {
+    $('#modal9').hide();//先隱藏視窗
+    $('.modal9').after('<div class="modal_overlay"></div>'); //新增透明底
+    $('.modal9').prepend('<button type="button" class="close">關閉</button>');  //新增關閉按鈕
+    $('.modal_overlay').hide();  //隱藏透明底
+    //按鈕動作
+    $('#openModal9').click(function(e) {
+        $('.modal_overlay').fadeIn(100);
+        $('.modal9').fadeIn(100);
+        $('body').addClass('noscroll');
+        e.preventDefault();
+    });
+    //關閉function
+    function closeModal(){
+        $('#modal9').hide();
+        $('.modal_overlay').hide();
+        $('body').removeClass('noscroll');
+    }
+    //點選關閉按鈕及透明底都可關閉
+    $('.modal_overlay').click(closeModal);
+    $('.modal9 .close').click(closeModal);
+
+});
+
+$(function() {
+    $('#modal10').hide();//先隱藏視窗
+    $('.modal10').after('<div class="modal_overlay"></div>'); //新增透明底
+    $('.modal10').prepend('<button type="button" class="close">關閉</button>');  //新增關閉按鈕
+    $('.modal_overlay').hide();  //隱藏透明底
+    //按鈕動作
+    $('#openModal10').click(function(e) {
+        $('.modal_overlay').fadeIn(100);
+        $('.modal10').fadeIn(100);
+        $('body').addClass('noscroll');
+        e.preventDefault();
+    });
+    //關閉function
+    function closeModal(){
+        $('#modal10').hide();
+        $('.modal_overlay').hide();
+        $('body').removeClass('noscroll');
+    }
+    //點選關閉按鈕及透明底都可關閉
+    $('.modal_overlay').click(closeModal);
+    $('.modal10 .close').click(closeModal);
+
+    $('#openModal11').click(function(){
+        $('#modal10').hide();
+    })
+
+
+});
+
+
+$(function() {
+    $('#modal11').hide();//先隱藏視窗
+    $('.modal11').after('<div class="modal_overlay"></div>'); //新增透明底
+    $('.modal11').prepend('<button type="button" class="close">關閉</button>');  //新增關閉按鈕
+    $('.modal_overlay').hide();  //隱藏透明底
+    //按鈕動作
+    $('#openModal11').click(function(e) {
+        $('.modal_overlay').fadeIn(100);
+        $('.modal11').fadeIn(100);
+        $('body').addClass('noscroll');
+        e.preventDefault();
+    });
+    //關閉function
+    function closeModal(){
+        $('#modal11').hide();
+        $('.modal_overlay').hide();
+        $('body').removeClass('noscroll');
+    }
+    //點選關閉按鈕及透明底都可關閉
+    $('.modal_overlay').click(closeModal);
+    $('.modal11 .close').click(closeModal);
+
+});
+
 
 
 //searchbox
@@ -223,3 +421,16 @@ $('.searchbox .btn-toggle').click(function(e) {
     });
     $(this).stop(true, true).toggleClass('close');
 });
+
+//ON(OFF)
+$(function(){
+    $('.switch .on').click(function(){
+        $('.switch .on').hide();
+        $('.switch .off').show();
+    })
+    $('.switch .off').click(function(){
+        $('.switch .off').hide();
+        $('.switch .on').show();
+    })
+})
+
