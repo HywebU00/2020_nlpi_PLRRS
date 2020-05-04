02ea71d571134066f3c33feae3ccba24a5b34420
mingshuya <mingshuya@gmail.com>
2020-05-04 10:55:50 +0800

    member3.html、member4.html更新

--- js/customize.js
+++ js/customize.js
@@ -523,3 +523,33 @@ $(function(){
     })
 })
 
+//ON(OFF)
+$(function(){
+    $('.switch2 .on').click(function(){
+        $('.switch2 .on').hide();
+        $('.switch2 .off').show();
+    })
+    $('.switch2 .off').click(function(){
+        $('.switch2 .off').hide();
+        $('.switch2 .on').show();
+    })
+})
+
+//狀態篩選手機
+$(function(){
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+      } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+      } 
+  });
+  }
+})
+
